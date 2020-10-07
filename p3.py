053675601071f3076df197f75a70b0dc04cc0415
bandar-X <bandar101.X@gmail.com>
2020-10-08 00:14:54 +0300

    second commit

---
+++ p3.py
@@ -0,0 +1,18 @@
+num1 = 5
+num2 = 6
+print(num1 > num2)
+print(5 > 4)
+print(5 != 4)
+print(True and True)
+print(True and False)
+print(False and False)
+
+
+
+print(True or True)
+print(False or False)
+print(True or False)
+
+
+print(not True)
+print(not False)
