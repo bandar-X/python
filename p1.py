e56ca14b7a20a3a28aaad43697be7eb46de145da
bandar-X <bandar101.X@gmail.com>
2020-10-08 03:09:01 +0300

    to commit

--- p1.py
+++ p1.py
@@ -1,102 +1,102 @@
-print(5+5)
-print(5-2)
-print(18/3)
-print(18./6.7)
-print(9%5)
-print(5*5*5+2)
-print(8**3)
-#/////////////////متحولات رقمية
-x=15
-x+10
-print(x)
-y=4
-print(x+y)
-m=1.5
-n=3.5
-print(m+n)
-#///////////////////متحولات نصية
-a="hello" 
-b="bandar"
-print(a,b)
-#//////////////////
-global e
-e=10
-print(e)
-#//////////////////////
-print(pow(3,4))
-import math
-print(math.floor(17.6))
-t=math.sqrt
-print(t(2))
-#////////////////////////
-mm=str(10)
-print("heloo bandar",mm)
-#////////////////////////
-str= 'heloo bandar'
-print(str)
-print (str[0])
-print(str[0:5])
-print(str[2:])
-print(str*2)
-print(str+"hi")
-#/////////////////////
-list=['bandar',50,'hi',52]
-smalllist =['ban',236]
-print(list)
-print(list[0])
-print(list[1:3])
-print(list+smalllist)
-print(list*3)
-#/////////////////////////
-tuple =("bandar",10,'mood','linux')
-tinytuple=('kali',15)
-print(tuple)
-print(tuple[0])
-print(tuple[1:3])
-print(tuple[2])
-print(tuple,tinytuple)
-print(tuple*2)
-#//////////////////////////////
-dict={'bandar':12 ,143:'ban'}
-print(dict[143])
-print(dict.keys())
-print(dict.values())
-#/////////////////////////////
-#x=3
-#o=6
-#y=5
-#if x==4:
- #print(x)
-#elif o==6:
- #print(o)
-#else:
- #print(y)
-#/////////////////////////
-i =1
-while i<9:
-    print(i)
-    i =i+2
-    #//////////////////////////
-    for letter in 'python':
-        print(letter)
-    pass
-#/////////////////////////////////
-def bandar(str):
+# print(5+5)
+# print(5-2)
+# print(18/3)
+# print(18./6.7)
+# print(9%5)
+# print(5*5*5+2)
+# print(8**3)
+# #/////////////////متحولات رقمية
+# x=15
+# x+10
+# print(x)
+# y=4
+# print(x+y)
+# m=1.5
+# n=3.5
+# print(m+n)
+# #///////////////////متحولات نصية
+# a="hello" 
+# b="bandar"
+# print(a,b)
+# #//////////////////
+# global e
+# e=10
+# print(e)
+# #//////////////////////
+# print(pow(3,4))
+# import math
+# print(math.floor(17.6))
+# t=math.sqrt
+# print(t(2))
+# #////////////////////////
+# mm=str(10)
+# print("heloo bandar",mm)
+# #////////////////////////
+# str= 'heloo bandar'
+# print(str)
+# print (str[0])
+# print(str[0:5])
+# print(str[2:])
+# print(str*2)
+# print(str+"hi")
+# #/////////////////////
+# list=['bandar',50,'hi',52]
+# smalllist =['ban',236]
+# print(list)
+# print(list[0])
+# print(list[1:3])
+# print(list+smalllist)
+# print(list*3)
+# #/////////////////////////
+# tuple =("bandar",10,'mood','linux')
+# tinytuple=('kali',15)
+# print(tuple)
+# print(tuple[0])
+# print(tuple[1:3])
+# print(tuple[2])
+# print(tuple,tinytuple)
+# print(tuple*2)
+# #//////////////////////////////
+# dict={'bandar':12 ,143:'ban'}
+# print(dict[143])
+# print(dict.keys())
+# print(dict.values())
+# #/////////////////////////////
+# #x=3
+# #o=6
+# #y=5
+# #if x==4:
+#  #print(x)
+# #elif o==6:
+#  #print(o)
+# #else:
+#  #print(y)
+# #/////////////////////////
+# i =1
+# while i<9:
+#     print(i)
+#     i =i+2
+#     #//////////////////////////
+#     for letter in 'python':
+#         print(letter)
+#     pass
+# #/////////////////////////////////
+# def bandar(str):
     
-    print(str)
+#     print(str)
     
-    bandar('ban')
-    #///////////////////////////
-    def num(x,y):
- print(x,y)
+#     bandar('ban')
+#     #///////////////////////////
+#     def num(x,y):
+#  print(x,y)
 
 
-num(3,4)
-#/////////////////////
-class example:
-    def bandar(self,name);
-    print(name)
-#//////////////////////////////
-num1 = 5
-num2 = 6
-print(num1 > num2)
+# num(3,4)
+# #/////////////////////
+# class example:
+#     def bandar(self,name);
+#     print(name)
+# #//////////////////////////////
+# num1 = 5
+# num2 = 6
+# print(num1 > num2)
--- p3.py
+++ p3.py
@@ -1,18 +1,21 @@
-num1 = 5
-num2 = 6
-print(num1 > num2)
-print(5 > 4)
-print(5 != 4)
-print(True and True)
-print(True and False)
-print(False and False)
+# num1 = 5
+# num2 = 6
+# print(num1 > num2)
+# print(5 > 4)
+# print(5 != 4)
+# print(True and True)
+# print(True and False)
+# print(False and False)
+
+# print(True or True)
+# print(False or False)
+# print(True or False)
+
+
+# print(not True)
+# print(not False)
 
 
 
-print(True or True)
-print(False or False)
-print(True or False)
-
-
-print(not True)
-print(not False)
+a,b = 1,2
+print(a)
